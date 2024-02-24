a38348c994e9c7bee0c3cc878333e244e6fd856c
JuanGiacalone <juanpablogiacalone8@gmail.com>
2024-02-24 01:56:49 -0300

    pagination and sorting done

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,9 +1,14 @@
 package example.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,9 +25,21 @@ class CashCardController {
         Optional<CashCard> cashCardOptional = cashCardRepository.findById(id);
         return cashCardOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
-    @GetMapping()
-    private ResponseEntity<Iterable<CashCard>> findAll() {
-        return ResponseEntity.ok(cashCardRepository.findAll());
+//    @GetMapping()
+//    private ResponseEntity<Iterable<CashCard>> findAll() {
+//
+//        return ResponseEntity.ok(cashCardRepository.findAll());
+//    }
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        // HERE I USE A PROVIDED OR A DEFAULT SORTING : ASC BY AMOUNT
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 
     @PostMapping
